0d57e51431723862a80bce65751d0419748c059d
rodrigopfelix <rodrigopfelix@hotmail.com>
2019-07-13 23:43:30 -0300

    Adicionado upload de arquivo no Firebase storage
    Signed-off-by: rodrigopfelix <rodrigopfelix@hotmail.com>

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var firebase = require('firebase');
 var bodyParser = require('body-parser');
+const {Storage} = require('@google-cloud/storage');
+const Multer = require('multer');
 
 var app = express();
+const storage = new Storage ();
+const bucket = storage.bucket("gs://drigo-api-project.appspot.com");
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024 // no larger than 5mb, you can change as needed.
+  }
+});
 app.use(bodyParser.json()); //need to parse HTTP request body
 
 const config = {
@@ -103,10 +113,61 @@ app.delete('/User/:userName', function (req, res) {
           });
 });
 
+app.post('/upload', multer.single('file'), (req, res) => {
+  console.log('Upload Image');
+
+  let file = req.file;
+  if (file) {
+    uploadImageToStorage(file).then((success) => {
+      res.status(200).send({
+        status: 'success'
+      });
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+});
+
+/**
+ * Upload the image file to Google Storage
+ * @param {File} file object that will be uploaded to Google Storage
+ */
+const uploadImageToStorage = (file) => {
+  return new Promise((resolve, reject) => {
+    if (!file) {
+      reject('No image file');
+    }
+    
+    let newFileName = `${file.originalname}_${Date.now()}`;
+
+    let fileUpload = bucket.file(newFileName);
+
+    const blobStream = fileUpload.createWriteStream({
+      metadata: {
+        contentType: file.mimetype
+      }
+    });
+
+    blobStream.on('error', (error) => {
+      reject('Something is wrong! Error: ' + error);
+    });
+
+    blobStream.on('finish', () => {
+      // The public URL can be used to directly access the file via HTTP.
+      // const url = format(`https://storage.googleapis.com/${bucket.name}/${fileUpload.name}`);
+      const url = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${fileUpload.name}?alt=media`;
+      console.log (`PUBLIC URL: ${url}`);
+      resolve(url);
+    });
+
+    blobStream.end(file.buffer);
+  });
+}
+
 var server = app.listen(8080, function () {
 
-   var host = server.address().address;
-   var port = server.address().port;
+  var host = server.address().address;
+  var port = server.address().port;
 
-   console.log("Example app listening at http://%s:%s", host, port);
+  console.log("Example app listening at http://%s:%s", host, port);
 });
